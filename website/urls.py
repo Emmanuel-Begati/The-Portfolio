2381ed7b8738e034e13f2e024d0d5559752c2795
StellaIbeh <a.ibeh@alustudent.com>
2025-03-02 23:12:49 +0200

    making progress

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('home1/', views.home1, name='home1'),
+    path('home2/', views.home2, name='home2'),
+    path('home3/', views.home3, name='home3'),
+    path('home4/', views.home4, name='home4'),
+    path('home5/', views.home5, name='home5'),
+    path('home6/', views.home6, name='home6'),
 ]
--- website/views.py
+++ website/views.py
@@ -3,4 +3,22 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'website/home.html')   
+    return render(request, 'website/index.html')   
+
+def home1 (request):
+    return render(request, 'website/home1.html')
+
+def home2 (request):
+    return render(request, 'website/home2.html')
+
+def home3 (request):
+    return render(request, 'website/home3.html') 
+
+def home4 (request):
+    return render(request, 'website/home4.html')
+
+def home5 (request):
+    return render(request, 'website/home5.html')
+
+def home6 (request):
+    return render(request, 'website/home6.html')
