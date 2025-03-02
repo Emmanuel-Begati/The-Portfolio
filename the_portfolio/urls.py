c2ee3f8c676268cd8cd965359da7f38f83a58d52
StellaIbeh <a.ibeh@alustudent.com>
2025-03-02 22:15:41 +0200

    made some edits

--- the_portfolio/urls.py
+++ the_portfolio/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 import website
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include(website.urls) )
+    path("", include("website.urls")),
     
 ]
