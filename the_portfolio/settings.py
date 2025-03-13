b74b3b93e31aa71f33027060aa449fadd91908c1
Emmanuel-Begati <begati16@gmail.com>
2025-03-13 20:12:31 +0200

    done

--- the_portfolio/settings.py
+++ the_portfolio/settings.py
@@ -122,3 +122,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# settings.py
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587  # Use 465 for SSL
+EMAIL_USE_TLS = True  # TLS encryption
+EMAIL_HOST_USER = 'begati16@gmail.com'  # Your Gmail address
+EMAIL_HOST_PASSWORD = 'qqcn vqiv nfyb pbth'  # Your Gmail password
